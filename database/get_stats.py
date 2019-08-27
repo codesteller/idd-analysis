a0c35a894c67a510ad49a88b8bcc491296f6cb94
codesteller <pallab.vsp@gmail.com>
2019-08-27 21:44:47 +0530

    adding stats - 2

--- database/get_stats.py
+++ database/get_stats.py
@@ -1,5 +1,6 @@
 from devlogger import devlogger
 import logging
+import pandas as pd
 
 
 class DBStats:
@@ -19,26 +20,79 @@ class DBStats:
         self.class_list = list()
 
         # Method Calls
+        self.get_class_list()
         self.stats = self.get_stats()
 
     def get_stats(self):
         """
+        @brief: This function extracts the following statistics from the dataset
+                -> statistics per image in dictionary with image name as key
+                -> overall object counts
+        :return: stats_dict
 
-        :return: stats_dict -> statistics per image in dictionary with image name as key
         """
         self.logger.log_i("Extracting statistics")
-        stats_dict = dict()
-
-        for idx in self.db:
-            temp_dict = dict()
-            ianno = self.db[idx]["annotation"]
-            _class_list_per_image = list()
-            for x in ianno:
-                if x[1].lower() not in self.class_list:
-                    self.class_list.append(x[1].lower())
-                if x[1].lower() not in _class_list_per_image:
-                    _class_list_per_image.append(x[1].lower())
-            temp_dict["class_list"] = _class_list_per_image
-            stats_dict[idx] = temp_dict
+        stats_dict = self.image_level_stats_dict()
+        stats_pd = self.image_level_stats_pd()
 
         return stats_dict
+
+    def image_level_stats_dict(self):
+        """
+        @brief: This function takes annotation of an image in a list and extracts objects and their counts in the image.
+        :return: stats_dict with extracted data
+        """
+        stats_dict = dict()
+        stats_dict["images"] = dict()
+
+        for im_name in self.db:
+            # Extract Image Level Statistics
+            ianno = self.db[im_name]["annotation"]
+            temp_dict = dict()
+            temp_dict["class_hist"] = dict()
+            _class_list_per_image = list()
+            for x in ianno:
+                class_name = x[1].lower()
+                if class_name not in self.class_list:
+                    self.class_list.append(x[1].lower())
+                if class_name not in _class_list_per_image:
+                    _class_list_per_image.append(x[1].lower())
+
+                if class_name in temp_dict["class_hist"]:
+                    temp_dict["class_hist"][class_name] = temp_dict["class_hist"][class_name] + 1
+                else:
+                    temp_dict["class_hist"][class_name] = 1
+            temp_dict["class_list"] = _class_list_per_image
+            stats_dict["images"][im_name] = temp_dict
+
+        return stats_dict
+
+    def get_class_list(self):
+        for idx in self.db:
+            # Get annotation object
+            ianno = self.db[idx]["annotation"]
+
+            for x in ianno:
+                class_name = x[1].lower()
+                if class_name not in self.class_list:
+                    self.class_list.append(class_name)
+        return
+
+    def image_level_stats_pd(self):
+        """
+
+        :return:
+        """
+        _temp_stats = dict()
+        image_names = list()
+
+        # make empty table for all classes per image
+        zeros_list = [0] * len(self.db)
+        for image_name in self.db:
+            ianno = self.db[image_name]["annotation"]
+            image_names.append(image_name)
+            for iclass in self.class_list:
+                _temp_stats[iclass] = zeros_list
+        _temp_stats["images"] = image_names
+
+        return _temp_stats
